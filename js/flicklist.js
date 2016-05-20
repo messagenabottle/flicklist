a72e72556430a251aed1381943cd2de1a1638074
Mark Praprotnik <emarkymark@gmail.com>
2016-05-20 17:09:56 +0000

    finish FlickList 5 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -2,16 +2,17 @@
 
 var model = {
   watchlistItems: [],
-  browseItems: []
+  browseItems: [],
 
   // TODO 
   // add a property for the current active movie index
+  activeMovieIndex: 0
 }
 
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "8e888fa39ec243e662e1fb738c42ae99", // TODO 0 add your api key
+  token: "4fb0028699136841cd735c4d378c0dcb", // TODO DONE 0 add your api key
   /**
    * Given a movie object, returns the url to its poster image
    */
@@ -83,7 +84,7 @@ function render() {
 
   // clear everything
   $("#section-watchlist ul").empty();
-  $("#section-browse ul").empty();
+  $("#section-browse .carousel-inner").empty();
 
   // render watchlist items
   model.watchlistItems.forEach(function(movie) {
@@ -122,28 +123,27 @@ function render() {
     $("#section-watchlist ul").append(itemView);
   });
 
-  // render browse items
-  model.browseItems.forEach(function(movie) {
-    var title = $("<h4></h4>").text(movie.original_title);
-    var overview = $("<p></p>").text(movie.overview);
-
-    // button for adding to watchlist
-    var button = $("<button></button>")
-      .text("Add to Watchlist")
-      .attr("class", "btn btn-primary")
-      .click(function() {
-        model.watchlistItems.push(movie);
-        render();
-      })
-      .prop("disabled", model.watchlistItems.indexOf(movie) !== -1);
-
-    var itemView = $("<li></li>")
-      .attr("class", "list-group-item")
-      .append( [title, overview, button] );
-      
-    // append the itemView to the list
-    $("#section-browse ul").append(itemView);
-  });
+  var activeMovie = model.browseItems[model.activeMovieIndex];
+  $('#browse-info h4').text(activeMovie.original_title);
+  $('#browse-info p').text(activeMovie.overview);
+  $('#add-to-watchlist')
+    .unbind('click')
+    .click(function() {
+      model.watchlistItems.push(activeMovie);
+      render();
+    })
+    .prop('disabled', model.watchlistItems.indexOf(activeMovie) !== -1);
+    
+  var posters = model.browseItems.map(function(movie) {
+    var poster = $("<img/>")
+      .attr("src", api.posterUrl(movie))
+      .attr("class", "img-responsive");
+    return $('<li></li>')
+    .attr('class', 'item')
+    .append(poster);
+  })
+  $('#section-browse .carousel-inner').append(posters);
+  posters[model.activeMovieIndex].addClass("active");
 }
 
 
@@ -152,3 +152,26 @@ function render() {
 $(document).ready(function() {
   discoverMovies(render);
 });
+
+  // render browse items
+  // model.browseItems.forEach(function(movie) {
+  //   var title = $("<h4></h4>").text(movie.original_title);
+  //   var overview = $("<p></p>").text(movie.overview);
+
+  //   button for adding to watchlist
+  //   var button = $("<button></button>")
+  //     .text("Add to Watchlist")
+  //     .attr("class", "btn btn-primary")
+  //     .click(function() {
+  //       model.watchlistItems.push(movie);
+  //       render();
+  //     })
+  //     .prop("disabled", model.watchlistItems.indexOf(movie) !== -1);
+
+  //   var itemView = $("<li></li>")
+  //     .attr("class", "list-group-item")
+  //     .append( [title, overview, button] );
+      
+  //   append the itemView to the list
+  //   $("#section-browse ul").append(itemView);
+  // });
